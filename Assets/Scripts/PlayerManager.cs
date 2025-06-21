ca2f261149e72258b576fb0b34dcc88b9de905d1
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-21 21:52:46 +0530

    Player Health UI Added

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,13 +1,16 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
     public float health = 100f;
+    public TextMeshProUGUI healthText;
 
     public void Hit(float damage)
     {
         health -= damage;
+        healthText.text = "Health: " + health.ToString("F0");
 
         if (health <= 0f)
         {

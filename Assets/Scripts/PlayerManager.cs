234beca815fd5b68bd1414ac28e0f71ed8cae504
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-06-22 21:31:09 +0530

    Camera Shake Added

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,30 @@ public class PlayerManager : MonoBehaviour
     public TextMeshProUGUI healthText;
 
     public GameManager gameManager;
+    public GameObject playerCamera;
+
+    private Quaternion playerCameraOriginalRotation;
+
+    private float shakeTime;
+    private float shakeDuration;
+
+    private void Start()
+    {
+        playerCameraOriginalRotation = playerCamera.transform.localRotation;
+    }
+
+    private void Update()
+    {
+        if (shakeTime < shakeDuration)
+        {
+            shakeTime += Time.deltaTime;
+            CameraShake();
+        }
+        else if (playerCamera.transform.localRotation != playerCameraOriginalRotation)
+        {
+            playerCamera.transform.localRotation = playerCameraOriginalRotation;
+        }
+    }
 
     public void Hit(float damage)
     {
@@ -17,5 +41,15 @@ public class PlayerManager : MonoBehaviour
         {
             gameManager.EndGame();
         }
+        else
+        {
+            shakeTime = 0f;
+            shakeDuration = 0.2f;
+        }
+    }
+
+    public void CameraShake()
+    {
+        playerCamera.transform.localRotation = Quaternion.Euler(Random.Range(-2, 2), 0, 0);
     }
 }
